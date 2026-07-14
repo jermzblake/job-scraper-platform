b8fa10a224c4b4954ce60f42394f20293db4c3e1
jermzblake <jermzblake@gmail.com>
2026-07-13 20:48:38 -0600

    Enhance parser module by adding HTML helper functions to __all__ exports for improved accessibility.

--- scrapers/parsers/__init__.py
+++ scrapers/parsers/__init__.py
@@ -1,3 +1,22 @@
 from .base import ParsedJob, parse_jobs
+from .html import (
+    attr_of,
+    iter_cards,
+    select_all,
+    select_one,
+    snippet_of,
+    soup_from_html,
+    text_of,
+)
 
-__all__ = ["ParsedJob", "parse_jobs"]
+__all__ = [
+    "ParsedJob",
+    "attr_of",
+    "iter_cards",
+    "parse_jobs",
+    "select_all",
+    "select_one",
+    "snippet_of",
+    "soup_from_html",
+    "text_of",
+]
